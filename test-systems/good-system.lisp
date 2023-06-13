09e8a5924f6365455e0c170c4365ce67df24e23d
Anton Vodonosov <avodonosov@yandex.ru>
2023-06-13 04:05:10 +0300

    testing issue #2 'ABCL failure'

--- test-systems/good-system.lisp
+++ test-systems/good-system.lisp
@@ -1,4 +1,4 @@
-(uiop:define-package good-system ;; temporarily switched to uiop:define-package, to test issue #2 "ABCL failure"
+(uiop:define-package #:good-system ;; temporarily switched to uiop:define-package and uninterned symbol, to test issue #2 "ABCL failure"
   (:use cl)
   (:export good-func-1
            good-func-2
