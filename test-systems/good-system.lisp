3ab65bc9e0df2134f588804c8db5faa3720c18d5
Anton Vodonosov <avodonosov@yandex.ru>
2023-06-13 03:32:26 +0300

    testing issue #2 'ABCL failure'

--- test-systems/good-system.lisp
+++ test-systems/good-system.lisp
@@ -1,4 +1,4 @@
-(defpackage good-system
+(defpackage #:good-system ;; temporarily made it an uninterned symbol, to test issue #2 "ABCL failure"
   (:use cl)
   (:export good-func-1
            good-func-2
